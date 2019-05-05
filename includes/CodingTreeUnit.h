7f46643065b1c53e8c0fb69820628a650d5c17d2
keisuke kamiya <contact@sanozi.com>
2019-05-05 12:55:32 +0900

    CodingTreeを作るクラスを作成

---
+++ includes/CodingTreeUnit.h
@@ -0,0 +1,14 @@
+//
+// Created by kasph on 2019/05/05.
+//
+
+#ifndef ENCODER_CODINGTREEUNIT_H
+#define ENCODER_CODINGTREEUNIT_H
+
+
+class CodingTreeUnit {
+
+};
+
+
+#endif //ENCODER_CODINGTREEUNIT_H
---
+++ src/CodingTreeUnit.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kasph on 2019/05/05.
+//
+
+#include "../includes/CodingTreeUnit.h"
