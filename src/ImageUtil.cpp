073a2df9750ba6342aafc4c1fa7b5ffbd28db57e
keisuke kamiya <contact@sanozi.com>
2019-05-23 12:52:40 +0900

    画像関係のUtilを隔離する

---
+++ src/ImageUtil.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kasph on 2019/05/23.
+//
+
+#include "ImageUtil.h"
---
+++ src/ImageUtil.h
@@ -0,0 +1,8 @@
+//
+// Created by kasph on 2019/05/23.
+//
+
+#ifndef ENCODER_IMAGEUTIL_H
+#define ENCODER_IMAGEUTIL_H
+
+#endif //ENCODER_IMAGEUTIL_H
