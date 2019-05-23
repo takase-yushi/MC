1ff7609258500c5037756f183bf0adacc5d5a4a4
keisuke kamiya <contact@sanozi.com>
2019-05-23 22:14:52 +0900

    GaussNewtonの戻り値をtupleに変更した

--- includes/ME.hpp
+++ includes/ME.hpp
@@ -64,6 +64,6 @@ std::pair<std::vector<cv::Point2f>, std::priority_queue<int>> getReferenceImageC
                                                                                            cv::Mat &debug);
 
 double getPredictedImage(cv::Mat& ref_image, cv::Mat& target_image, cv::Mat& output_image, Point3Vec& triangle, std::vector<cv::Point2f>& mv, bool parallel_flag);
-std::pair<std::vector<cv::Point2f>, cv::Point2f> GaussNewton(cv::Mat ref_image, cv::Mat target_image, cv::Mat gauss_ref_image, Point3Vec target_corners);
+std::tuple<std::vector<cv::Point2f>, cv::Point2f, bool> GaussNewton(cv::Mat ref_image, cv::Mat target_image, cv::Mat gauss_ref_image, Point3Vec target_corners);
 
 #endif //ENCODER_ME_H
