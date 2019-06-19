83dd81ff5a4ced8e4ed2e1cb55a4d649fa85fb24
keisuke <contact@sanozi.com>
2019-06-20 00:58:55 +0900

    マーカート法とブロックマッチング法を分離

--- includes/ME.hpp
+++ includes/ME.hpp
@@ -56,6 +56,8 @@ std::pair<std::vector<cv::Point2f>, std::priority_queue<int>> getReferenceImageC
                                                                                            cv::Mat &debug);
 
 double getPredictedImage(cv::Mat& ref_image, cv::Mat& target_image, cv::Mat& output_image, Point3Vec& triangle, std::vector<cv::Point2f>& mv, bool parallel_flag, std::vector<std::vector<int>> &area_flag, int triangle_index, CodingTreeUnit *ctu, cv::Rect block_size);
+std::tuple<std::vector<cv::Point2f>, cv::Point2f, double, int, bool> Marquardt(std::vector<std::vector<cv::Mat>> ref_images, std::vector<std::vector<cv::Mat>> target_images, std::vector<std::vector<std::vector<unsigned char **>>> expand_image, Point3Vec target_corners, const std::vector<std::vector<int>> &area_flag, int triangle_index, CodingTreeUnit *ctu, int block_size_x, int block_size_y);
+
 std::tuple<std::vector<cv::Point2f>, cv::Point2f, double, int, bool> GaussNewton(std::vector<std::vector<cv::Mat>> ref_images, std::vector<std::vector<cv::Mat>> target_images, std::vector<std::vector<std::vector<unsigned char **>>> expand_image, Point3Vec target_corners, const std::vector<std::vector<int>> &area_flag, int triangle_index, CodingTreeUnit *ctu, int block_size_x, int block_size_y);
 
 double bicubic_weight(double x);
