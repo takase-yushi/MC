9db596f795d592af1310955de1fdfebd47f1fa5c
Keisuke <contact@sanozi.com>
2019-07-05 19:39:02 +0900

    三角形の残差を返すgetTriangleResidualを追加

--- src/ImageUtil.cpp
+++ src/ImageUtil.cpp
@@ -112,6 +112,56 @@ double getTriangleResidual(const cv::Mat ref_image, const cv::Mat &target_image,
     return squared_error;
 }
 
+
+/***
+ * @fn double getTriangleResidual(const cv::Mat &target_image, const cv::Mat ref_image, Point3Vec vec)
+ * @brief ある三角形を変形した際の残差を返す
+ * @param ref_image 参照画像
+ * @param target_image 対象画像
+ * @param triangle 三角パッチの座標
+ * @param vec 動きベクトル
+ * @return 残差
+ */
+double getTriangleResidual(unsigned char **ref_image, const cv::Mat &target_image, Point3Vec &triangle, std::vector<cv::Point2f> mv, const std::vector<cv::Point2f> &in_triangle_pixels, cv::Rect rect){
+    cv::Point2f pp0, pp1, pp2;
+
+    pp0.x = triangle.p1.x + mv[0].x;
+    pp0.y = triangle.p1.y + mv[0].y;
+    pp1.x = triangle.p2.x + mv[1].x;
+    pp1.y = triangle.p2.y + mv[1].y;
+    pp2.x = triangle.p3.x + mv[2].x;
+    pp2.y = triangle.p3.y + mv[2].y;
+
+    cv::Point2f X,a,b,a_later,b_later,X_later;
+    double alpha,beta,det;
+
+    double squared_error = 0.0;
+
+    a = triangle.p3 - triangle.p1;
+    b = triangle.p2 - triangle.p1;
+    det = a.x * b.y - a.y * b.x;
+
+    for(const auto& pixel : in_triangle_pixels) {
+        X.x = pixel.x - triangle.p1.x;
+        X.y = pixel.y - triangle.p1.y;
+        alpha = (X.x * b.y - X.y * b.x) / det;
+        beta = (a.x * X.y - a.y * X.x) / det;
+        X.x += triangle.p1.x;
+        X.y += triangle.p1.y;
+
+        a_later = pp2 - pp0;
+        b_later = pp1 - pp0;
+        X_later = alpha * a_later + beta * b_later + pp0;
+
+        int y = img_ip(ref_image, rect, X_later.x, X_later.y, 2);
+
+        squared_error += pow((M(target_image, (int)pixel.x, (int)pixel.y) - (0.299 * y + 0.587 * y + 0.114 * y)), 2);
+    }
+
+    return squared_error;
+}
+
+
 /**
  * @fn std::vector<std::vector<cv::Mat>> getRefImages(const cv::Mat ref_image, const cv::Mat gauss_ref_image)
  * @brief 参照画像の集まりを返す
