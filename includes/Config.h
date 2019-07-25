d686888bb92eb7faecb9aa9325382836b47553ee
Kamiya Keisuke <ozilikepop@gmail.com>
2019-07-25 12:21:23 +0900

    設定を保持するConfigクラス追加 #81

---
+++ includes/Config.h
@@ -0,0 +1,14 @@
+//
+// Created by Kamiya Keisuke on 2019-07-25.
+//
+
+#ifndef ENCODER_CONFIG_H
+#define ENCODER_CONFIG_H
+
+
+class Config {
+
+};
+
+
+#endif //ENCODER_CONFIG_H
---
+++ src/Config.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kamiya Keisuke on 2019-07-25.
+//
+
+#include "../includes/Config.h"
