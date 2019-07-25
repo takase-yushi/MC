1e727ed788a5db6f5e14a65782bbed6ac65a2c6b
takaseyushi <yushi.minecraft@gmail.com>
2019-07-25 10:50:44 +0900

    blockMatching(四角形用)の座標の4倍が間違っていたので修正。 #52

--- src/ME.cpp
+++ src/ME.cpp
@@ -183,19 +183,19 @@ std::tuple<std::vector<cv::Point2f>, double> blockMatching(Point4Vec sq, const c
     sp3 = sq.p3;
     sp4 = sq.p4;
 
-    sp1.x = (sp1.x + 1) * 4 - 1;
-    sp1.y = (sp1.y + 1) * 4 - 1;
-    sp2.x = (sp2.x + 1) * 4 - 1;
-    sp2.y = (sp2.y + 1) * 4 - 1;
-    sp3.x = (sp3.x + 1) * 4 - 1;
-    sp3.y = (sp3.y + 1) * 4 - 1;
-    sp4.x = (sp4.x + 1) * 4 - 1;
-    sp4.y = (sp4.y + 1) * 4 - 1;
+//    sp1.x = (sp1.x + 1) * 4 - 1;
+//    sp1.y = (sp1.y + 1) * 4 - 1;
+//    sp2.x = (sp2.x + 1) * 4 - 1;
+//    sp2.y = (sp2.y + 1) * 4 - 1;
+//    sp3.x = (sp3.x + 1) * 4 - 1;
+//    sp3.y = (sp3.y + 1) * 4 - 1;
+//    sp4.x = (sp4.x + 1) * 4 - 1;
+//    sp4.y = (sp4.y + 1) * 4 - 1;
 
-    sx = std::min({sp1.x, sp2.x, sp3.x, sp4.x});
-    sy = std::min({sp1.y, sp2.y, sp3.y, sp4.y});
-    lx = std::max({sp1.x, sp2.x, sp3.x, sp4.x});
-    ly = std::max({sp1.y, sp2.y, sp3.y, sp4.y});
+    sx = 4 * std::min({sp1.x, sp2.x, sp3.x, sp4.x});
+    sy = 4 * std::min({sp1.y, sp2.y, sp3.y, sp4.y});
+    lx = 4 * std::max({sp1.x, sp2.x, sp3.x, sp4.x}) + 3;
+    ly = 4 * std::max({sp1.y, sp2.y, sp3.y, sp4.y}) + 3;
 
     cv::Point2f mv_tmp(0.0, 0.0); //ブロックの動きベクトル
     int SX = 16; // ブロックマッチングの探索範囲(X)
@@ -219,7 +219,7 @@ std::tuple<std::vector<cv::Point2f>, double> blockMatching(Point4Vec sq, const c
                 e_count = 0;
                 for (int y = (int) (round(sy) / 4); y <= (int) (round(ly) / 4); y++) {
                     for (int x = (int) (round(sx) / 4); x <= (int) (round(lx) / 4); x++) {
-                        e += fabs(R(expansion_image, i + 4 * x + spread_quarter, j + 4 * y + spread_quarter) - M(current, x, y));
+                        e += fabs(R(expansion_image, i + 4 * x + spread_quarter, j + 4 * y + spread_quarter) - R(current, x, y));
                         e_count++;
                     }
                 }
@@ -248,7 +248,7 @@ std::tuple<std::vector<cv::Point2f>, double> blockMatching(Point4Vec sq, const c
                 e_count = 0;
                 for (int y = (int) (round(sy) / 4); y <= (int) (round(ly) / 4); y++) {
                     for (int x = (int) (round(sx) / 4); x <= (int) (round(lx) / 4); x++) {
-                        e += fabs(R(expansion_image, i + 4 * x + spread_quarter, j + 4 * y + spread_quarter) - M(current, x, y));
+                        e += fabs(R(expansion_image, i + 4 * x + spread_quarter, j + 4 * y + spread_quarter) - R(current, x, y));
                         e_count++;
 
                     }
@@ -277,7 +277,7 @@ std::tuple<std::vector<cv::Point2f>, double> blockMatching(Point4Vec sq, const c
                 e_count = 0;
                 for (int y = (int) (round(sy) / 4); y <= (int) (round(ly) / 4); y++) {
                     for (int x = (int) (round(sx) / 4); x <= (int) (round(lx) / 4); x++) {
-                        e += fabs(R(expansion_image, i + 4 * x + spread_quarter, j + 4 * y + spread_quarter) - M(current, x, y));
+                        e += fabs(R(expansion_image, i + 4 * x + spread_quarter, j + 4 * y + spread_quarter) - R(current, x, y));
                         e_count++;
                     }
                 }
