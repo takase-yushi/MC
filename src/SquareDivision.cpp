46a528869277ba58667668716e1577e684fdbfa1
takaseyushi <yushi.minecraft@gmail.com>
2019-07-26 15:16:30 +0900

    画像上の四角パッチにのみ番号をふる。 #52

--- src/SquareDivision.cpp
+++ src/SquareDivision.cpp
@@ -77,9 +77,9 @@ void SquareDivision::initSquare(int _block_size_x, int _block_size_y, int _divid
 
     previousMvList.emplace_back();
     // すべての頂点を入れる
-    for(int block_y = 0 ; block_y <= block_num_y ; block_y++) {
+    for(int block_y = 0 ; block_y < block_num_y ; block_y++) {
         //y列目の上側の頂点を入れる
-        for (int block_x = 0 ; block_x <= block_num_x; block_x++) {
+        for (int block_x = 0 ; block_x < block_num_x; block_x++) {
             int nx = block_x * (block_size_x);    //ブロックの左上のx座標
             int ny = block_y * (block_size_y);    //ブロックの左上のy座標
 
@@ -115,7 +115,7 @@ void SquareDivision::initSquare(int _block_size_x, int _block_size_y, int _divid
 
         if(block_y == block_num_y) continue;    //y座標が画像の端のときはcontinue
         //y列目の下側の頂点を入れる
-        for (int block_x = 0 ; block_x <= block_num_x; block_x++) {
+        for (int block_x = 0 ; block_x < block_num_x; block_x++) {
             int nx = block_x * (block_size_x);              //ブロックの左下のx座標
             int ny = (block_y + 1) * (block_size_y) - 1;    //ブロックの左下のy座標
 
