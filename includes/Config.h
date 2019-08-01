02845ad59fd18292d1157b499cc216d5583c47b1
keisuke <contact@sanozi.com>
2019-08-02 01:31:50 +0900

    設定ファイルの内容を保持するConfigクラス追加 #81

--- includes/Config.h
+++ includes/Config.h
@@ -6,8 +6,57 @@
 #define ENCODER_CONFIG_H
 
 
+#include <string>
+
 class Config {
 
+
+private:
+    std::string img_directory;
+    std::string gauss_ref_image;
+    std::string ref_image;
+    std::string target_image;
+    int qp;
+    int ctu_width;
+    int ctu_height;
+    int division_step;
+public:
+    const std::string &getImgDirectory() const;
+
+    const std::string &getGaussRefImage() const;
+
+    const std::string &getRefImage() const;
+
+    const std::string &getTargetImage() const;
+
+    int getQp() const;
+
+    int getCtuWidth() const;
+
+    int getCtuHeight() const;
+
+    int getDivisionStep() const;
+
+public:
+    void setImgDirectory(const std::string &imgDirectory);
+
+    void setGaussRefImage(const std::string &gaussRefImage);
+
+    void setRefImage(const std::string &refImage);
+
+    void setTargetImage(const std::string &targetImage);
+
+    void setQp(int qp);
+
+    void setCtuWidth(int ctuWidth);
+
+    void setCtuHeight(int ctuHeight);
+
+    void setDivisionStep(int divisionStep);
+
+public:
+    Config(const std::string &imgDirectory, const std::string &gaussRefImage, const std::string &refImage,
+           const std::string &targetImage, int qp, int ctuWidth, int ctuHeight, int divisionStep);
 };
 
 
--- src/Config.cpp
+++ src/Config.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "../includes/Config.h"
+
+Config::Config(const std::string &imgDirectory, const std::string &gaussRefImage, const std::string &refImage,
+               const std::string &targetImage, int qp, int ctuWidth, int ctuHeight, int divisionStep) : img_directory(
+        imgDirectory), gauss_ref_image(gaussRefImage), ref_image(refImage), target_image(targetImage), qp(qp),
+        ctu_width(ctuWidth), ctu_height(ctuHeight), division_step(divisionStep) {}
+
+void Config::setImgDirectory(const std::string &imgDirectory) {
+    img_directory = imgDirectory;
+}
+
+void Config::setGaussRefImage(const std::string &gaussRefImage) {
+    gauss_ref_image = gaussRefImage;
+}
+
+void Config::setRefImage(const std::string &refImage) {
+    ref_image = refImage;
+}
+
+void Config::setTargetImage(const std::string &targetImage) {
+    target_image = targetImage;
+}
+
+void Config::setQp(int qp) {
+    Config::qp = qp;
+}
+
+void Config::setCtuWidth(int ctuWidth) {
+    ctu_width = ctuWidth;
+}
+
+void Config::setCtuHeight(int ctuHeight) {
+    ctu_height = ctuHeight;
+}
+
+void Config::setDivisionStep(int divisionStep) {
+    division_step = divisionStep;
+}
+
+const std::string &Config::getImgDirectory() const {
+    return img_directory;
+}
+
+const std::string &Config::getGaussRefImage() const {
+    return gauss_ref_image;
+}
+
+const std::string &Config::getRefImage() const {
+    return ref_image;
+}
+
+const std::string &Config::getTargetImage() const {
+    return target_image;
+}
+
+int Config::getQp() const {
+    return qp;
+}
+
+int Config::getCtuWidth() const {
+    return ctu_width;
+}
+
+int Config::getCtuHeight() const {
+    return ctu_height;
+}
+
+int Config::getDivisionStep() const {
+    return division_step;
+}
