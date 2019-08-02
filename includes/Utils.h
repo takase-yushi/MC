e9a11e87de965033909a4f32af885b6e90b546f7
Keisuke <contact@sanozi.com>
2019-08-02 21:32:18 +0900

    ガウスニュートンの初期値はデフォルトでオフにします

--- includes/Utils.h
+++ includes/Utils.h
@@ -83,7 +83,7 @@ enum {
 
 #define PRED_MODE NEWTON
 #define GAUSS_NEWTON_PARALLEL_ONLY false
-#define GAUSS_NEWTON_INIT_VECTOR true
+#define GAUSS_NEWTON_INIT_VECTOR false
 #define GAUSS_NEWTON_HEVC_IMAGE true
 #define HEVC_REF_IMAGE true
 #define STORE_MVD_DISTRIBUTION_LOG true
