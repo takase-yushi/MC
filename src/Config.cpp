55e57775a288e79c969e27f3852d015ec9dde906
Keisuke <contact@sanozi.com>
2019-08-21 15:37:30 +0900

    ログ吐き出し先を保持しておくlog_directoryを追加 #138

--- src/Config.cpp
+++ src/Config.cpp
@@ -4,9 +4,9 @@
 
 #include "../includes/Config.h"
 
-Config::Config(bool enableFlag, const std::string &imgDirectory, const std::string &gaussRefImage, const std::string &refImage,
+Config::Config(bool enableFlag, const std::string &imgDirectory, const std::string &logDirectory, const std::string &gaussRefImage, const std::string &refImage,
                const std::string &targetImage, int qp, int ctuWidth, int ctuHeight, int divisionStep) : enable(enableFlag), img_directory(
-        imgDirectory), gauss_ref_image(gaussRefImage), ref_image(refImage), target_image(targetImage), qp(qp),
+        imgDirectory), log_directory(logDirectory), gauss_ref_image(gaussRefImage), ref_image(refImage), target_image(targetImage), qp(qp),
         ctu_width(ctuWidth), ctu_height(ctuHeight), division_step(divisionStep) {}
 
 const std::string &Config::getImgDirectory() const {
@@ -43,4 +43,8 @@ int Config::getDivisionStep() const {
 
 bool Config::isEnable() const {
     return enable;
+}
+
+const std::string &Config::getLogDirectory() const {
+    return log_directory;
 }
