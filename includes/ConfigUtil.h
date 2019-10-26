c79d52a1379fad83b0a4fe6f695a255df834ac29
keisuke <contact@sanozi.com>
2019-10-26 18:45:03 +0900

    コンフィグファイルを複数のファイルに分割して吐き出す関数追加

--- includes/ConfigUtil.h
+++ includes/ConfigUtil.h
@@ -10,5 +10,6 @@
 
 std::vector<Config> readTasks(std::string config_name = "config.json");
 void generateConfigItem(std::string input_file_path, std::string output_file_path);
+void generateChunkedConfigItem(std::string input_file_path, std::string output_file_path, int chunk_size);
 
 #endif //ENCODER_CONFIGUTIL_H
