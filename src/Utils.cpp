2dada04d3b778797d76315feccae0d9c6ac57221
takaseyushi <yushi.minecraft@gmail.com>
2019-11-08 12:14:26 +0900

    drawPointが全然ポイント(点)じゃなくてうざい。 #52

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -83,10 +83,10 @@ void drawSquare(cv::Mat &img, const cv::Point2f p1, const cv::Point2f p2, const 
         cv::line(img, p3, p1, color, 1);
     }
     if(select == 1) {
-        drawPoint(img, p1, RED, 1);
-        drawPoint(img, p2, RED, 1);
-        drawPoint(img, p3, RED, 1);
-        drawPoint(img, p4, RED, 1);
+        cv::line(img, p1, p1, RED, 1);
+        cv::line(img, p2, p2, RED, 1);
+        cv::line(img, p3, p3, RED, 1);
+        cv::line(img, p4, p4, RED, 1);
     }
 
 }
