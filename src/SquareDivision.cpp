6d741b878868d3f0aacd00141301e26b516026ab
takaseyushi <yushi.minecraft@gmail.com>
2019-11-15 12:23:44 +0900

    右上にブロックがない判定をミスってた。 #52

--- src/SquareDivision.cpp
+++ src/SquareDivision.cpp
@@ -1632,7 +1632,9 @@ std::tuple< std::vector<std::vector<std::pair<cv::Point2f, MV_CODE_METHOD >>>, s
         }
     }
     else if(tmp_reference_block.size() == 4) {
-        if(flag){  //sp4がない場合
+        cv::Point2f sp4 = corners[squares[square_idx].p2_idx];
+        sp4.x++; sp4.y--;
+        if(getCornerIndex(sp4) == -1){  //右上にブロックがない場合
             if(translation_flag) {
                 //①
                 if (is_in_flag[0] && is_in_flag[1] && tmp_merge_vectors[0] == tmp_merge_vectors[1])
