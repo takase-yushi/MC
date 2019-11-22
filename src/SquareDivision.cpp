57968a9796ec4dc0b45ee28fb2c9d296ffa99f9b
takaseyushi <yushi.minecraft@gmail.com>
2019-11-22 14:10:28 +0900

    RDコスト再計算時にctuに入れるものはここでは入れない。 #52

--- src/SquareDivision.cpp
+++ src/SquareDivision.cpp
@@ -1065,21 +1065,9 @@ bool SquareDivision::split(std::vector<std::vector<std::vector<unsigned char **>
         ctu->split_cu_flag = true;
         for (int j = 0; j < (int) subdiv_target_squares.size(); j++) {
             // j個目の四角形
-            ctus[j]->square_index = square_indexes[j];
-            if (split_mv_result[j].translation_flag) {
-                ctus[j]->mv1 = split_mv_result[j].mv_translation;
-                ctus[j]->mv2 = split_mv_result[j].mv_translation;
-                ctus[j]->mv3 = split_mv_result[j].mv_translation;
-            } else {
-                ctus[j]->mv1 = split_mv_result[j].mv_warping[0];
-                ctus[j]->mv2 = split_mv_result[j].mv_warping[1];
-                ctus[j]->mv3 = split_mv_result[j].mv_warping[2];
-            }
-            ctus[j]->translation_flag = split_mv_result[j].translation_flag;
-            ctus[j]->method = method_flags[j];
             square_gauss_results[square_indexes[j]] = split_mv_result[j];
             if (method_flags[j] == MV_CODE_METHOD::MERGE) {
-                if (ctus[j]->translation_flag) {
+                if (split_mv_result[j].translation_flag) {
                     square_gauss_results[square_indexes[j]].mv_translation = original_mv_translation[j];
                 } else {
                     square_gauss_results[square_indexes[j]].mv_warping = original_mv_warping[j];
