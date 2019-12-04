85039b3614234de322639546a75f3e833cecb161
Keisuke <contact@sanozi.com>
2019-12-04 22:09:34 +0900

    【デバッグ用】パッチが何で符号化されているかを示す定数を追加

--- includes/CodingTreeUnit.h
+++ includes/CodingTreeUnit.h
@@ -18,6 +18,13 @@ enum MV_CODE_METHOD {
     MERGE2
 };
 
+enum PATCH_CODING_MODE {
+    TRANSLATION_DIFF = 0,
+    TRANSLATION_MERGE,
+    AFFINE_DIFF,
+    AFFINE_MERGE,
+    AFFINE_NEW_MERGE
+};
 
 class CodingTreeUnit {
 public:
