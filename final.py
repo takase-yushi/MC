820dc12d7d64a88a518419151f89d33e4a1d61cf
Keisuke <contact@sanozi.com>
2020-01-29 13:32:38 +0900

    俺は6並列で行くッ

--- final.py
+++ final.py
@@ -28,7 +28,7 @@ directories_4k = [
 indices = [x for x in range (1, 9 * 30 + 1)]
 
 for dir_name in directories_2k:
-    Parallel(n_jobs=1)([delayed(func)("config/" + dir_name + str(i) + ".json") for i in indices])
+    Parallel(n_jobs=6)([delayed(func)("config/" + dir_name + str(i) + ".json") for i in indices])
 
 #
 # for dir_name in directories_4k:
